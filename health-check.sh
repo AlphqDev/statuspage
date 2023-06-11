8010541c053e8bbe0aeb87c0d8e3cad1d696d818
AlphqDev <96098521+AlphqDev@users.noreply.github.com>
2023-06-11 22:17:39 +0900

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -59,8 +59,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config user.name github-actions[bot]
-  git config user.email github-actions[bot]@users.noreply.github.com
+  git config --global user.name 'NVHost Bot'
+  git config --global user.email 'bot@hostnv.kr'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
